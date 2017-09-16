e4a3c7067d6b691f42d468d0ec0bfd5570c46131
Peter1295537 <datkoolkidpeter@gmail.com>
2017-09-16 14:45:20 -0400

    Added AccountSUmmary activity

---
+++ app/src/main/java/com/example/peter/oashiba/AccountSummary.java
@@ -0,0 +1,13 @@
+package com.example.peter.oashiba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_summary);
+    }
+}
--- app/src/main/java/com/example/peter/oashiba/LogIn.java
+++ app/src/main/java/com/example/peter/oashiba/LogIn.java
@@ -7,6 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+
 public class LogIn extends AppCompatActivity {
 
     @Override
@@ -15,6 +23,8 @@ public class LogIn extends AppCompatActivity {
         setContentView(R.layout.activity_log_in);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        signInButtonListener();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +35,25 @@ public class LogIn extends AppCompatActivity {
             }
         });
     }
+// Listern for Sign In button
+    public void signInButtonListener() {
+
+        Button button = (Button) findViewById(R.id.button_id);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                //Intent browserIntent =
+                //        new Intent(Intent.ACTION_VIEW, ));
+                //startActivity(browserIntent);
+
+            }
+
+        });
+
+    }
+
 
 }
