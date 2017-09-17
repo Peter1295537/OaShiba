c045d75e244d78b66b13e5f8f74890c7ccb63117
Peter1295537 <datkoolkidpeter@gmail.com>
2017-09-16 20:15:18 -0400

    Minor changes to capitalOneConfirmation to hide password

--- app/src/main/java/com/example/peter/oashiba/CapitalOneConfirmation.java
+++ app/src/main/java/com/example/peter/oashiba/CapitalOneConfirmation.java
@@ -1,13 +1,20 @@
 package com.example.peter.oashiba;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
+
+
 
 public class CapitalOneConfirmation extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,30 @@ public class CapitalOneConfirmation extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button signIn = (Button) findViewById(R.id.button_id);
+        signIn.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View arg0) {
+
+                // get EditText by id and store it into "inputTxt"
+                EditText inputTxt = (EditText) findViewById(R.id.capOneSignin);
+
+                // Store EditText - Input in variable
+
+                String name = inputTxt.getText().toString();
+                if (name.equals("Colton Krajcik")){
+                    logIn();
+                }
+
+            }
+
+        });
+
+    }
+
+    public void logIn() {
+        Intent myIntent = new Intent(this, AccountSummary.class);
+        startActivity(myIntent);
     }
 
 }
